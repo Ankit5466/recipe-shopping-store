8be34bede2e76a4d0e97b253f6ad27d0eb387237
Ankit5466 <ankitsharma5466@gmail.com>
2024-02-11 22:48:06 +0530

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Recipe Name', 'Recipe Desc', 'https://www.foodandwine.com/thmb/dX7pNh_WX83ESqb9VJuvkBwVKwM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg'), 
+    new Recipe('Recipe Name', 'Recipe Desc', 'https://www.foodandwine.com/thmb/dX7pNh_WX83ESqb9VJuvkBwVKwM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Braciole-FT-RECIPE1122-66acf49cef0e4390bec780945709e7f3.jpg')
+  ];
+
+  constructor() {}
+}
